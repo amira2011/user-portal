4bb105cc2220a760bbd0327d3db3b8ffa27524c9
amira2011 <shaikh.abidali@gmail.com>
2024-02-18 21:57:08 +0530

    Bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "You have Successfully Signed Up"
-      redirect_to(:users, notice: "User was successfully created")
+      redirect_to root_path
     else
       render "new", status: :unprocessable_entity
     end

1f588d8a1c5f76d5c77fec7a1344dbc7ad5d4fa4
amira2011 <shaikh.abidali@gmail.com>
2024-03-02 16:40:52 +0530

    leads

--- app/controllers/lead_details_controller.rb
+++ app/controllers/lead_details_controller.rb
@@ -6,10 +6,9 @@ class LeadDetailsController < ApplicationController
   end
 
   def show
-    @names = ["home_garage", "home_owner", "home_length", "interested_in_home_insurance", "interested_in_condo_insurance", "interested_in_life_insurance", "interested_in_renters_insurance", "interested_in_usage_based_policy", "currently_insured", "current_company", "current_customer", "continuous_coverage", "current_policy_expiration_date", "military_affiliation"]
+    # @names = ["home_garage", "home_owner", "home_length", "interested_in_home_insurance", "interested_in_condo_insurance", "interested_in_life_insurance", "interested_in_renters_insurance", "interested_in_usage_based_policy", "currently_insured", "current_company", "current_customer", "continuous_coverage", "current_policy_expiration_date", "military_affiliation"]
     @lead_detail = LeadDetail.find_by(lead_id: params[:id])
-    lead_id = params[:id]
-    @lead_drivers = 
-    puts "ok"
+    @lead_drivers = @lead_detail.lead.lead_drivers
+    
   end
 end

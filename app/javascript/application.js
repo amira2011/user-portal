e5903459d0f7957078572bce07c4154ab08684af
amira2011 <shaikh.abidali@gmail.com>
2024-03-08 20:14:19 +0530

    ok

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,31 +1,19 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "popper"
-import "bootstrap"
-import "jquery"
-import "jquery_ujs"
-import "./jquery_ui"
+import "@hotwired/turbo-rails";
+import "controllers";
+import "popper";
+import "bootstrap";
+import "jquery";
+import "jquery_ujs";
+import "./jquery_ui";
 //import "@fortawesome/fontawesome-free";
 
-$("#notice").fadeOut(4000)
+$("#notice").fadeOut(4000);
 
-$("#alert").fadeOut(4000)
+$("#alert").fadeOut(4000);
 
-
- 
-
-
-
-  
-      
- $(document).on('click', '.nav-menu .menu li ', function () {
-            console.log($(this))
-           $('.nav-menu .menu li').removeClass('active');
-      
-    
-      $(this).addClass('active');
-      });    
-
-
- 
+$(document).on("click", ".nav-menu .menu li ", function () {
+  console.log($(this));
+  $(".nav-menu .menu li").removeClass("active");
+  $(this).addClass("active");
+});

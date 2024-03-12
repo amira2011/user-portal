76681b6c02e4b45b4688710f9340e366bae46979
amira2011 <shaikh.abidali@gmail.com>
2024-03-12 22:36:13 +0530

    Resourceful Driver

--- app/controllers/lead_drivers_controller.rb
+++ app/controllers/lead_drivers_controller.rb
@@ -3,16 +3,21 @@ class LeadDriversController < ApplicationController
   before_action :require_login
 
   def edit
+    @lead = Lead.find(params[:lead_id])
     @lead_driver = LeadDriver.find(params[:id])
+    # @lead_driver = LeadDriver.find(params[:id])
     respond_to do |format|
       format.js # This will look for a file named edit.js.erb
     end
   end
 
   def update
+    @lead = Lead.find(params[:lead_id])
     @lead_driver = LeadDriver.find(params[:id])
+    # @lead_driver = LeadDriver.find(params[:id])
     if @lead_driver.update(lead_driver_params)
-      redirect_to @lead_driver, notice: "Lead driver was successfully updated."
+      # redirect_to @lead_driver, notice: "Lead driver was successfully updated."
+      redirect_to lead_lead_driver_path(@lead, @lead_driver), notice: "Driver was successfully updated."
     else
       render :edit, status: :unprocessable_entity
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   root "leads#index"
   resources :users
-  resources :leads
+  # resources :leads
+
+  resources :leads do
+    resources :lead_drivers
+  end
+
   resources :lead_details
-  resources :lead_drivers
+  # resources :lead_drivers
   get "login" => "user_sessions#new", :as => :login
   post "login" => "user_sessions#create"
   post "logout" => "user_sessions#destroy", :as => :logout

f1982c86ca275d291ebecd20217239205fc07e7c
amira2011 <shaikh.abidali@gmail.com>
2024-03-13 18:27:35 +0530

    Relationship

--- app/models/lead_driver.rb
+++ app/models/lead_driver.rb
@@ -1,5 +1,18 @@
 class LeadDriver < ApplicationRecord
-    belongs_to :lead
-    has_many :lead_vehicles
-    has_many :lead_violations
+  belongs_to :lead
+  has_many :lead_vehicles
+  has_many :lead_violations
+
+  VALID_RELATIONSHIPS = [
+    "self",
+    "spouse",
+    "parent",
+    "sibling",
+    "child",
+    "grandparent",
+    "grandchild",
+    "domestic partner",
+    "other",
+  ].freeze
+  #   validates :relationship, inclusion: { in: VALID_RELATIONSHIPS, message: "%{value} is not a valid relationship" }
 end
